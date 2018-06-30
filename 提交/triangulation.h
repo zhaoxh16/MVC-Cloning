fc1beba1a7f890d505895556f8338b9f4d037761
zxh <zhaoxinhao2012@126.com>
2018-06-30 10:10:47 +0800

    提交

--- 提交/triangulation.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <opencv2/opencv.hpp>
-#include "mesh.h"
-using namespace std;
-
-void triangulation(vector<Point>& boundaryPoints, vector<Point>& vertices, vector<Mesh>& meshs, const Mat& mask);
--- 提交/源.cpp
+++
@@ -1,8 +0,0 @@
-#include "Image.h"
-
-int main() {
-	Image image;
-	image.readImage("ship.bmp","target2.jpg",Point(250,200));
-	image.init();
-	return 0;
-}
